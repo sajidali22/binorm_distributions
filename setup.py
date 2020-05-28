a7325c3cd20b94ce3abcc2addd13daee11838ffd
Sajid Ali <52592276+sajidali22@users.noreply.github.com>
2020-05-29 01:48:10 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binorm_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['binorm_distributions'],
+      zip_safe=False)
